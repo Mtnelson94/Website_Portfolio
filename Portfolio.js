9b891e3c6bd6412d0711d93d03f49fda251ce2bb
Michael Nelson <code>
2022-01-13 12:06:00 -0500

    cant ref CSS for some reason

--- Portfolio.js
+++ Portfolio.js
@@ -1,8 +1,8 @@
-function click() {
-    let x = document.getElementById("Skills")
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        
-    }
-}
+// function Myclick() {
+//     var x = document.getElementById("Myskills")
+//     if (x.style.display === "none") {
+//         x.style.display = "block";
+//     } else {
+//         x.style.display = "none";
+//     }
+// }
